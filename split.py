d3bda072ca01398af93f6a34e8026348d8ef5d8f
Mariana Graterol Fuenmayor <mgrafu@gmail.com>
2021-02-26 16:42:55 +0000

    Take arguments to split and write train, dev and test files.

--- split.py
+++ split.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Tokenizes a corpus and splits into test, dev and training files.
 
 This script takes a file as an imput, tokenizes it and randomizes it
 to divide it into three files with names given in the command line.
-These files will have the corpus divided into 80%, 20% and 20%,
+These files will have 80%, 10% and 10% of the corpus content, respectively,
 so the script can be used to create train, dev and test files.
 
 To check for accuracy, the randomizer is seeded with the number ####.
@@ -12,6 +12,7 @@ To check for accuracy, the randomizer is seeded with the number ####.
 import argparse
 
 
+from math import floor
 from typing import Iterator, List
 
 
@@ -36,17 +37,37 @@ def write_tags(source: Iterator[List[List[str]]], path: str):
             for word in line:
                 statement = ' '.join(word)
                 print(statement, file=target)
-            if line is not source[-1]:
-                print('', file=target)
+            print('', file=target)
 
 
 def main(args: argparse.Namespace) -> None:
-    corpus = list(read_tags("conll2000.tag"))
-    write_test = corpus[:2]
-    write_tags(write_test, 'writetest.tag')
+    corpus_name = args.input
+    corpus = list(read_tags(corpus_name))
+    corpus_len = len(corpus)
+    
+    train_file_name = args.train
+    train_file_EOF_corpus_index = floor(corpus_len / 10 * 8)
+    train_file_data = corpus[:train_file_EOF_corpus_index]
+    write_tags(train_file_data, train_file_name)
+    
+    dev_file_name = args.dev
+    dev_file_BOF_corpus_index = floor(corpus_len / 10 * 8)
+    dev_file_EOF_corpus_index = floor(corpus_len / 10 * 9)
+    dev_file_data = corpus[dev_file_BOF_corpus_index:dev_file_EOF_corpus_index]
+    write_tags(dev_file_data, dev_file_name)
+
+    test_file_name = args.test
+    test_file_BOF_corpus_index = floor(corpus_len / 10 * 9)
+    test_file_data = corpus[test_file_BOF_corpus_index:]
+    write_tags(test_file_data, test_file_name)
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # TODO: declare arguments.
+    parser.add_argument("input", type=str, help="File name for the corpus to be used.")
+    parser.add_argument("train", type=str, help="File name for the training data file to be written.")
+    parser.add_argument("dev", type=str, help="File name for the dev data file to be written.")
+    parser.add_argument("test", type=str, help="File name for the testing data file to be written.")
+
     main(parser.parse_args())
